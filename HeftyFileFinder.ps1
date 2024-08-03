e489c5d549bf02d1e16055169e98dd31388c3ee4
ShellNinja96 <diogoestrela96@gmail.com>
2024-08-03 13:07:42 +0100

    First Commit

---
+++ HeftyFileFinder.ps1
@@ -0,0 +1,345 @@
+$splashTitle = @"
+ |   |         _|  |               ____| _)  |            ____| _)             |             
+ |   |   _ \  |    __|  |   |      |      |  |   _ \      |      |   __ \   _  |   _ \   __| 
+ ___ |   __/  __|  |    |   |      __|    |  |   __/      __|    |  |   |  (   |   __/  |    
+_|  _| \___| _|   \__| \__, |     _|     _| _| \___|     _|     _| _|  _| \__,_| \___| _|    
+                       ____/`n
+"@
+
+#================================================================================================================================================#
+#=SETTINGS=======================================================================================================================================#
+
+$skipFolders = @(
+    "C:\Example\Folder",
+    "C:\Other\Example\Folder"
+)
+
+$verboseSuccess = $false
+$verboseFails   = $true
+$verboseSkips   = $true
+
+$logSuccess = $true
+$logFails   = $true
+$logSkips   = $true
+
+# HTML settings
+$font            = "Cascadia Mono"
+$backgroundColor = "#202020"
+$foregroundColor = "#f2f2f2"
+$headerColor     = "#77c387"
+
+$gradient0       = "#F2F2F2"
+$gradient1       = "#F1DFE0"
+$gradient2       = "#F0CCCF"
+$gradient3       = "#EEB9BD"
+$gradient4       = "#EDA6AB"
+$gradient5       = "#EC929A"
+$gradient6       = "#EB7F88"
+$gradient7       = "#E96C76"
+$gradient8       = "#E85965"
+$gradient9       = "#E74653"
+
+
+#================================================================================================================================================#
+#=CLASSES========================================================================================================================================#
+
+class Path {
+
+    [string]$path
+    [int64]$size
+    [bool]$isDirectory
+    [array]$subPaths
+    [bool]$failed
+    [float]$occupancy
+    
+    Path([string]$path) { 
+        $this.path = $path
+        $this.size = 0
+        $this.isDirectory = $false
+        $this.subPaths = @()
+        $this.failed = $false
+        $this.occupancy = 0
+    }
+
+}
+
+#================================================================================================================================================#
+#=FUNCTIONS======================================================================================================================================#
+
+function LogFail {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$LogPath,
+        [Parameter(Mandatory=$true)]
+        [string]$FailedPath,
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.ErrorRecord]$ErrorRecord
+    )
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $errorMessage = $ErrorRecord.Exception.Message
+    $errorDetails = $ErrorRecord.ScriptStackTrace
+    $errorLocation = $ErrorRecord.InvocationInfo.PositionMessage
+
+    $logEntry = "[$timestamp] [FAILED]  $FailedPath`nErrorMessage: $errorMessage`nDetails: $errorDetails`nLocation: $errorLocation`n"
+    
+    Add-Content -Path $LogPath -Value $logEntry
+}
+
+#================================================================================================================================================#
+
+function LogMessage {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Type,
+        [Parameter(Mandatory=$true)]
+        [string]$Message,
+        [Parameter(Mandatory=$true)]
+        [string]$LogPath
+    )
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "[$timeStamp] [$Type] $Message"
+
+    Add-Content -Path $LogPath -Value $logEntry
+}
+
+#================================================================================================================================================#
+
+function PathSearch {
+    Param(
+        [parameter(Mandatory=$true)]
+        [string]$RootPath
+    )
+
+    Write-Host "`nSCAN STARTED..." -ForegroundColor Green
+    
+    function Recurse {
+        Param(
+            [parameter(Mandatory=$true)]
+            [string]$CurrentPath
+        )
+
+        # Skip specified folders
+        if ($skipFolders -contains $CurrentPath) {
+            if ($verboseSkips) { Write-Host "[SKIPPED] " -NoNewLine -ForegroundColor Yellow; Write-Host $CurrentPath }
+            if ($logSkips)     { LogMessage -Type "SKIPPED" -Message $CurrentPath -LogPath $logPath }
+            return $null
+        }
+
+        # Create the Current Path Object
+        $currentPathObject = [Path]::new($CurrentPath)
+
+        # Determine if the Current Path Object is a directory
+        try { $currentItem = Get-ChildItem -Path $CurrentPath -ErrorAction Stop } catch { 
+            $currentPathObject.failed = $true
+            if ($verboseFails) { Write-Host "[FAILED]  " -NoNewLine -ForegroundColor Red; Write-Host $CurrentPath }
+            if ($logFails)     { LogFail -LogPath $logPath -FailedPath $CurrentPath -ErrorRecord $_ }
+            return $currentPathObject
+        }
+
+        # If it is a directory...
+        if ($currentItem.PSIsContainer) { 
+
+            $currentPathObject.isDirectory = $true
+
+            # Get all children of the CurrentPath add them to the subPaths array of the Current Path Object and sum their sizes to the size property of the Current Path Object
+            try {
+                Get-ChildItem -Path $CurrentPath -ErrorAction Stop | ForEach-Object {
+                    $currentItemChildren = Recurse -CurrentPath $_.FullName
+                    if ($null -ne $currentItemChildren) {
+                        $currentPathObject.subPaths += $currentItemChildren
+                        $currentPathObject.size += $currentItemChildren.size
+                    }
+                }
+                if ($verboseSuccess) { Write-Host "[SCANNED] " -NoNewLine -ForegroundColor Green; Write-Host $CurrentPath }
+                if ($logSuccess)     { LogMessage -Type "SCANNED" -Message $CurrentPath -LogPath $logPath }
+
+            } catch {
+                if ($verboseFails) { Write-Host "[FAILED]  " -NoNewLine -ForegroundColor Red; Write-Host $CurrentPath }
+                if ($logFails)     { LogFail -LogPath $logPath -FailedPath $CurrentPath -ErrorRecord $_ }
+            }
+
+            # Sort the subPaths array by size of the subPaths
+            $currentPathObject.subPaths = $currentPathObject.subPaths | Sort-Object -Property size -Descending
+
+            # Calculate the occupancy of each subPath in the CurrentPathObject
+            foreach ($subPathObject in $currentPathObject.subPaths) {
+                if($currentPathObject.size -ne 0) { $subPathObject.occupancy = $subPathObject.size * 100 / $currentPathObject.size }
+            }
+
+         
+        } else {
+
+            $currentPathObject.size = [System.IO.FileInfo]::new($CurrentPath).Length
+            if ($verboseSuccess) { Write-Host "[SCANNED] " -NoNewLine -ForegroundColor Green; Write-Host $CurrentPath }
+            if ($logSuccess)     { LogMessage -Type "SCANNED" -Message $CurrentPath -LogPath $logPath }
+
+        }
+
+        return $currentPathObject
+
+    }
+
+    $rootPathObject = Recurse -CurrentPath $RootPath
+    $rootPathObject.occupancy = 100
+
+    Write-Host "SCAN FINISHED!" -ForegroundColor Green
+    
+    return $rootPathObject
+
+}
+
+#================================================================================================================================================#
+
+function GetGradient {
+    Param(
+        [parameter(Mandatory=$true)]
+        [float]$Value
+    )
+
+    switch($Value) {
+        0  { return $gradient0 }
+        1  { return $gradient1 } 
+        2  { return $gradient2 } 
+        3  { return $gradient3 } 
+        4  { return $gradient4 } 
+        5  { return $gradient5 } 
+        6  { return $gradient6 } 
+        7  { return $gradient7 } 
+        8  { return $gradient8 } 
+        9  { return $gradient9 } 
+    }
+
+}
+
+#================================================================================================================================================#
+
+function ExportHTML {
+    Param(
+        [parameter(Mandatory=$true)]
+        [Path]$Results,
+        [parameter(Mandatory=$true)]
+        [string]$HTMLPath
+    )
+
+    $htmlHeader = @"
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Hefty File Finder - Results</title>
+        <style>
+            body {
+                font-family: '$font';
+                background-color: $backgroundColor;
+                color: $foregroundColor;
+            }
+            h1, h4 {
+                color: $headerColor;
+            }
+            .directory, .file {
+                cursor: pointer;
+                margin: 5px 0;
+                display: flex;
+                align-items: center;
+            }
+            .nested {
+                display: none;
+                margin-left: 20px;
+            }
+            .active {
+                display: block;
+            }
+            .icon {
+                width: 1.5em;
+                height: 1.5em;
+                margin-right: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>HEFTY FILE FINDER RESULTS</h1>
+        <h4>[HOST] $(hostname)</br>[TIME] $startTime</h4><hr color="$headerColor"></br>
+        
+"@
+
+    $htmlFooter = @"
+        <script>
+            document.querySelectorAll('.directory').forEach(dir => {
+                dir.addEventListener('click', function() {
+                    const nested = this.nextElementSibling;
+                    nested.classList.toggle('active');
+                    const icon = this.querySelector('img.icon');
+                    const occupancyIndex = parseFloat(this.getAttribute('occupancy-index'));
+                    if (nested.classList.contains('active')) {
+                        icon.src = ``icons/grad`${occupancyIndex}/folderOpen.png``;
+                    } else {
+                        icon.src = ``icons/grad`${occupancyIndex}/folder.png``;
+                    }
+                });
+            });
+        </script>
+    </body>
+    </html>
+"@
+
+    function Recurse {
+        Param(
+            [parameter(Mandatory=$true)]
+            [Path]$CurrentPathObject,
+            [parameter(Mandatory=$false)]
+            [string]$Indent
+        )
+
+        $occupancy = [Math]::Round($CurrentPathObject.occupancy, 2)
+        $occupancyIndex = [math]::Floor($occupancy / 10)
+        if($occupancyIndex -eq 10) { $occupancyIndex = 9 }
+        $occupancyColor = GetGradient -Value $occupancyIndex
+
+        if ($CurrentPathObject.isDirectory) {
+            
+            $htmlBuilder.Append("<div class='directory' occupancy-index='$occupancyIndex'>$Indent<img src='icons/grad$occupancyIndex/folder.png' class='icon'><span style='color: $occupancyColor;'>$($CurrentPathObject.path)<b>&nbsp;[SIZE] $([Math]::Round($CurrentPathObject.size / 1MB, 2)) MB [OCCUPANCY] $occupancy%</span></b></div>`n<div class='nested'>`n") | Out-Null
+            foreach ($pathObject in $CurrentPathObject.subPaths) {
+                Recurse -CurrentPathObject $pathObject -Indent ($Indent + "&nbsp;&nbsp;&nbsp;&nbsp;")
+            }
+            $htmlBuilder.Append("</div>`n") | Out-Null
+
+        } else {
+
+            $htmlBuilder.Append("<div class='file'>$Indent<img src='icons/grad$occupancyIndex/file.png' class='icon'><span style='color: $occupancyColor;'>$($CurrentPathObject.path) <b> [SIZE] $([Math]::Round($CurrentPathObject.size / 1MB, 2)) MB [OCCUPANCY] $([Math]::Round($CurrentPathObject.occupancy, 2))%</span></b></div>`n") | Out-Null
+        }
+
+    }
+
+    Write-Host "EXPORTING HTML..." -ForegroundColor Green
+
+    $htmlBuilder = New-Object System.Text.StringBuilder
+    $htmlBuilder.Append($htmlHeader) | Out-Null
+    Recurse -CurrentPathObject $Results 
+    $htmlBuilder.Append($htmlFooter) | Out-Null
+    [System.IO.File]::WriteAllText($HTMLPath, $htmlBuilder.ToString())
+
+    Write-Host "EXPORTED HTML!`n" -ForegroundColor Green
+
+}
+
+#================================================================================================================================================#
+#=MAIN=SCRIPT====================================================================================================================================#
+
+Write-Host $splashTitle -ForegroundColor Green
+
+$rootPath = Read-Host "Enter Search Path"
+if (-not(Test-Path $rootPath)) { Write-Host "Inexistent path" -ForegroundColor Red; exit }
+
+$startTime = Get-Date -Format "yyyy-MM-dd_HH.mm.sss"
+$logPath   = Join-Path -Path (Get-Location) -ChildPath "$(hostname)_$startTime.log"
+$htmlPath  = Join-Path -Path (Get-Location) -ChildPath "$(hostname)_$startTime.html"
+
+$searchResults = PathSearch -RootPath $rootPath
+ExportHTML -Results $searchResults -HTMLPath $htmlPath
+
+Write-Host "HTML exported to: $htmlPath"
+if (Test-Path $logPath) { Write-Host "Log exported to:  $logPath" }
+Write-Host ""
---
+++ README.md
@@ -0,0 +1,46 @@
+# HEFTY FILE FINDER
+Hefty File Finder is a PowerShell script designed to recursively scan every sub-directory and file of a given path, collecting size information about every found file and directory and sorting the results by size. The results of the scan are then exported into an HTML file, organized into expandable/collapsible divs, sorted by size, and colored by an occupancy index. Optionally, the script can also output a comprehensible and customizable log file.
+
+## Usage
+
+Open PowerShell in a terminal emulator, change the directory to the directory where the script is located, execute the script, and provide the absolute root path you want to scan. The results will be output into the current working directory. To analyze the results, open the resulting HTML file and click on any directory path to expand/collapse it. Size is represented in megabytes, and occupancy represents how much space a file/directory proportionally occupies in the directory it resides.
+
+## Settings
+
+**Settings are defined in variables at the top of the script itself:**
+
+* **$skipFolders**
+    - Array of absolute paths which you don't want the script to scan.
+* **$log***__<Success/Fails/Skips>__*
+    - True/false values, set which kind of information you want to be logged.
+* **$verbose***__<Success/Fails/Skips>__*
+    - True/false values, set which kind of information you want to be verbosed in the console.
+* **$font**
+    - Font to be used in the results HTML file 
+* **$*****<background/foreground/header>***__Color__
+    - *background* - background color for the whole page.
+    - *foreground* - color for unstyled text in the page (unused).
+    - *header* - color for text in the heder section of the page.
+* **$gradient***__<0-9>__*
+    - Occupancy colors, 0 is 0% to 9.999...%, 1 is 10% to 19.999...%, 9 is 90% to 100%. This only affects text, not icons.
+
+## FAQs
+* **Icons are not being displayed in the HTML results file.**
+>
+The HTML file must be in the same directory as the icons directory. For example, if the icons directory path is *C:\HeftyFileFinder\icons*, the HTML file path must be *C:\HeftyFileFinder\hostname_date_time*.html.
+
+* **How do I change the gradient in icons?**
+>
+Create your own icons and import them into the icons directory. The icons directory is composed of subdirectories named 'grad0' to 'grad9', with each containing icons for their respective gradient/occupancy index. All of these subdirectories of the icons directory contain 3 icons named ***folder.png*** (collapsed directory), ***folderOpen.png*** (expanded directory) and ***file.png***.
+
+* **The script is too slow! What do I do?**
+>
+Set ***$verboseSuccess*** and **$logSuccess*** to ***$false***. Still slow? Disable all other verbose and logging settings. Still slow? Your hardware limitations are not my problem.
+
+* **There is already other software that does this and more.**
+>
+Like any other software that does X. This is a minimalistic, portable, customizable, free and open-source alternative.
+
+* **Can I run this on Linux, MacOS, etc...?**
+>
+The script was written and tested for PowerShell 5.1 to maximize compatibility with modern Windows operating systems. PowerShell 5.1 runs on top of the .NET framework, hence, it is unlikely to be compatible with versions of PowerShell that run on .NET Core.
